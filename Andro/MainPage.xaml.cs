f7a89089bce68c32880a64561d6a521805db0881
nik.obukhov.2002 <nik.obukhov.2002@mail.ru>
2022-09-22 08:51:24 +0500

    1

---
+++ Andro/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace Andro
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private string GetHash(string input)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            return BitConverter.ToString(hash).Replace("-", "");
+        }
+
+        private string _isAuth = "2";
+        private async void AuthClicked(object sender, EventArgs e)
+        {
+            /*            string postParameters = "login=" + GetHash(LoginUser.Text) + "&password=" + GetHash(PassUser.Text);
+                        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://apis.api-mauijobs.site/Auth?" + postParameters);
+                        request.Method = "POST";
+                        request.ContentType = "application/x-www-form-urlencoded";
+                        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                        using (Stream stream = response.GetResponseStream())
+                        {
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                _isAuth = reader.ReadToEnd();
+                            }
+                        }
+                        response.Close(); */
+
+            if (_isAuth != "")
+            {
+                //var obj = JsonConvert.DeserializeObject<User>(_isAuth);
+                var obj = new User()
+                {
+                    Password = "2",
+                    Login = "1",
+                    ID = 2,
+                    Name = "ABa",
+                    Surname = "11",
+                    Patronomic = "fff",
+                    Enterprice = "addd.",
+                    DateofBirth = new DateTime()
+                };
+                await this.Navigation.PushModalAsync(new Menu(obj));
+                //переход
+            }
+        }
+    }
+}
---
+++ Andro/Menu.xaml.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Andro
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : ContentPage
+    {
+        User user;
+        public Menu(User user)
+        {
+            InitializeComponent();
+            this.user = user;
+            Updating(user);
+        }
+
+        private void SaveData(object sender, EventArgs e)
+        {
+            SaveUser();
+        }
+
+        private void UpdateBut(object sender, EventArgs e)
+        {
+            Updating(user);
+        }
+
+        private void Updating(User user)
+        {
+            TextName.Text = user.Name;
+            TextSurname.Text = user.Surname;
+            TextPatronomic.Text = user.Patronomic;
+            TextDateofBirth.Text = user.DateofBirth.ToString();
+            TextEnterprice.Text = user.Enterprice;
+        }
+
+        private void SaveUser()
+        {
+           /* bool isOkay;
+            string jsonString = JsonSerializer.Serialize<User>(new User()
+            {
+                ID = user.ID,
+                Name = TextName.Text,
+                Surname = TextSurname.Text,
+                Patronomic = TextPatronomic.Text,
+                DateofBirth = DateTime.Parse(TextDateofBirth.Text),
+                Enterprice = TextEnterprice.Text,
+            });
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://apis.api-mauijobs.site/Users?" + jsonString);
+            request.Method = "POST";
+            request.ContentType = "text/json";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    isOkay = Convert.ToBoolean(reader.ReadToEnd());
+                }
+            }
+            response.Close();*/
+
+
+        }
+
+    }
+}
